32f8dd3ce0e8f1e82e52697b3ebdd9af3252873b
WendyKariuki <phaminuswendy@gmail.com>
2024-05-20 20:30:26 +0300

    P3 functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
+greet_programmer()
 
 def greet(name):
+    print (f"Hello, {name}!")
     pass
+greet("Wendy")
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
-
+greet_with_default()
 def add(num1, num2):
+    return num1 + num2
     pass
+add(45, 55)
 
 def halve(number):
+    return(number / 2)
     pass
+halve(99.5)
+
